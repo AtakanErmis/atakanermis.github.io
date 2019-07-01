b441c37ababe66278f5a5196675b1202445eb7df
AtakanErmis <ataerms@gmail.com>
2019-07-01 17:53:51 +0300

    abov

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Anime from "react-anime";
-import "./Header.sass";
 
 const Header = () => {
-  return (
-    <header>
-      <Anime delay={500} duration={1000} opacity={[0, 1]}>
-        <h1>ATAKAN ERMİŞ</h1>
-      </Anime>
-    </header>
-  );
+    return (
+        <header>
+            <Anime delay={500} duration={1000} opacity={[0, 1]}>
+                <h1>ATAKAN ERMİŞ</h1>
+            </Anime>
+        </header>
+    );
 };
 
 export default Header;
