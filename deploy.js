e9e4bc273adc34fb4d28438d78b1bcdf9f6778cb
Atakan <ataerms@gmail.com>
2020-06-19 11:58:24 +0300

    deploy script

--- deploy.js
+++ deploy.js
@@ -1,6 +1,7 @@
 const ghpages = require('gh-pages');
 
+console.log("Starting to deploy into gh-pages.")
 ghpages.publish('public', (err) => {
     if (err) throw err
-    console.log("Deployment to gh-pages success.")
+    console.log("Deployment to gh-pages successful.")
 })
