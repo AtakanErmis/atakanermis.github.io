a57bd4eb8171836916a490198c19666b63850ef1
Atakan <ataerms@gmail.com>
2020-06-19 16:06:52 +0300

    remove deploy.js (unneeded)

--- deploy.js
+++
@@ -1,7 +0,0 @@
-const ghpages = require('gh-pages');
-
-console.log("Starting to deploy into gh-pages.")
-ghpages.publish('public', (err) => {
-    if (err) throw err
-    console.log("Deployment to gh-pages successful.")
-})
